1b27fd2ee56b09dea81a804e93db6369aecc7b4c
Insaf Garaev <46156711-gifgifinsaf@users.noreply.replit.com>
2025-11-04 08:51:30 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python310
-    pkgs.python310Packages.aiogram
-    pkgs.python310Packages.flask
-    pkgs.python310Packages.python-dotenv  # если используете .env
-  ];
-}
